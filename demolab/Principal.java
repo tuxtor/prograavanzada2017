53d78b55329ab55501a9f026e20797bf7b1f3f57
Victor Orozco <vorozco@nabenik.com>
2017-08-18 08:12:48 -0600

    Agregando primer ejemplo java

---
+++ demolab/Principal.java
@@ -0,0 +1,11 @@
+public class Principal{
+	
+	public static void main(String args[]){
+	
+		Automovil ferrari = new Automovil();
+		ferrari.setMarca("Ferrari");
+		ferrari.setAnno(2007);
+		System.out.println(ferrari.toString());
+	}
+
+}
