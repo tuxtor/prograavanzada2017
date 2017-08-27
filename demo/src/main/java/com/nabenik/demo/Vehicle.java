c73dfe5779d1b205a41531a8391f60018395a6e0
Víctor Orozco <vorozco@nabenik.com>
2017-08-27 14:39:53 -0600

    Adding inheritance sample

---
+++ demo/src/main/java/com/nabenik/demo/Vehicle.java
@@ -0,0 +1,39 @@
+package com.nabenik.demo;
+
+public class Vehicle {
+	
+	
+	
+	public Vehicle() {
+		super();
+	}
+	public Vehicle(String color, String brand, int model) {
+		super();
+		this.color = color;
+		this.brand = brand;
+		this.model = model;
+	}
+	
+	public String color;
+	public String brand;
+	public int model;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getModel() {
+		return model;
+	}
+	public void setModel(int model) {
+		this.model = model;
+	}
+
+}
