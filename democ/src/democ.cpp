92fad075baa8a22a9b578adcb9e7b00a210fdc2d
Víctor Orozco <vorozco@nabenik.com>
2017-09-27 05:02:31 -0600

    Adding memory and heap demos

--- democ/src/democ.cpp
+++ democ/src/democ.cpp
@@ -9,21 +9,59 @@
 #include <stdlib.h>
 #include <iostream>
 #include "funciones.h"
+#include "demoheap.h"
 using namespace std;
 
-
-int main() {
-	int a = 7, b = 8;
-
-//	demostrarPuntero();
-
-	printf("a=%d b=%d\n",a,b);
-
-	intercambia(&a,&b);
-	printf("a=%d b=%d\n",a,b);
-	return 0;
+void testArray() {
+	int foo[] = { 16, 2, 77, 40, 12071 };
+	for (int i = 0; i < 5; i++) {
+		cout << foo[i] << endl;
+	}
 }
 
+void testArray2() {
+	int foo[] = { 16, 2, 77, 40, 12071 };
+	for (int i = 0; i < 5; i++) {
+		int* dir = &foo[i];
+		cout << dir << endl;
+		cout << foo[i] << endl;
+	}
+}
 
+void testArray3() {
+	int foo[] = { 16, 2, 77, 40, 12071 };
+	int* ptr = &foo[0];
 
+	for (int i = 0; i < 5; i++) {
+		cout << *(ptr + i) << endl;
+	}
+}
+
+void testArray4() {
+	const char* szString = "Panchito";
+	cout << "The array is '" << szString << "'" << endl;
+	cout << "Display the string as an array: ";
+	for (int i = 0; i < 5; i++) {
+		cout << szString[i];
+	}
+	cout << endl;
+// now using typical pointer arithmetic
+	cout << "Display string using a pointer: ";
+	const char* pszString = szString;
+	while(*pszString)
+	{
+		cout << *pszString;
+		pszString++;
+	}
+	cout << endl;
+// wait until user is ready before terminating program
+	// to allow the user to see the program results
+	cout << "Press Enter to continue..." << endl;
+	cin.ignore(10, '\n');
+	cin.get();
+}
+
+int main() {
+	testArray4();
+}
 
---
+++ democ/src/demoheap.h
@@ -0,0 +1,16 @@
+/*
+ * demoheap.h
+ *
+ *  Created on: Sep 27, 2017
+ *      Author: tuxtor
+ */
+
+#ifndef DEMOHEAP_H_
+#define DEMOHEAP_H_
+
+double* child();
+void parent();
+
+
+
+#endif /* DEMOHEAP_H_ */
