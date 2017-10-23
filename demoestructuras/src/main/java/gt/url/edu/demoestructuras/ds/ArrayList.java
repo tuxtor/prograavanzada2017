c0472ed4e635422e3129686abb184cd6bce78802
Víctor Orozco <vorozco@nabenik.com>
2017-10-23 00:28:47 -0600

    Adding dynamic arraylist implementation

---
+++ demoestructuras/src/main/java/gt/url/edu/demoestructuras/ds/ArrayList.java
@@ -0,0 +1,73 @@
+package gt.url.edu.demoestructuras.ds;
+
+public class ArrayList<E> implements List<E> {
+
+	public static final int CAPACITY = 16;
+	private E[] data;
+	private int size = 0;
+
+	public ArrayList() {
+		this(CAPACITY);
+	}
+
+	public ArrayList(int capacity) {
+		data = (E[]) new Object[capacity];
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public E get(int i) {
+		checkIndex(i, size);
+		return data[i];
+	}
+
+	public E set(int i, E e) {
+		checkIndex(i, size);
+		E temp = data[i];
+		data[i] = e;
+		return temp;
+	}
+
+	public void add(int i, E e) {
+		checkIndex(i, size + 1);
+		if (size == data.length) // not enough capacity
+			resize(2 * data.length);
+		for (int k = size - 1; k >= i; k--) // start by shifting rightmost
+			data[k + 1] = data[k];
+		data[i] = e; // ready to place the new element size++;
+
+	}
+
+	public E remove(int i) throws IndexOutOfBoundsException {
+		checkIndex(i, size);
+		E temp = data[i];
+		for (int k = i; k < size - 1; k++)
+			data[k] = data[k + 1];
+		data[size - 1] = null;
+		size--;
+		return temp;
+	}
+
+	protected void checkIndex(int i, int n) throws IndexOutOfBoundsException {
+		if (i < 0 || i >= n)
+			throw new IndexOutOfBoundsException("Illegal index: " + i);
+	}
+
+	/**
+	 * Internal method to increase array capacity
+	 * @param capacity
+	 */
+	protected void resize(int capacity) {
+		E[] temp = (E[]) new Object[capacity];
+		for (int k=0; k < size; k++)
+			temp[k] = data[k];
+		data = temp;
+	}
+
+}
